a52d499a30804d7e66553534e54b4c3d60a01d49
Samir Anghan <samir.anghan@gmail.com>
2022-06-13 22:17:49 -0400

    JIRA-1200: Initial override methods

--- Git-Example/Git-Example/ViewController.swift
+++ Git-Example/Git-Example/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        
+    }
 
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
 
 }
 
