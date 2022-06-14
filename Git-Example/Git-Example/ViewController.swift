a4c4a7e39e3517ff901fd53c0778a543ec371ace
Samir Anghan <samir.anghan@gmail.com>
2022-06-13 21:29:05 -0400

    JIRA-1200: Setup initial project files

---
+++ Git-Example/Git-Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Git-Example
+//
+//  Created by Dev on 6/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Git-Example/Git-ExampleTests/Git_ExampleTests.swift
@@ -0,0 +1,33 @@
+//
+//  Git_ExampleTests.swift
+//  Git-ExampleTests
+//
+//  Created by Dev on 6/13/22.
+//
+
+import XCTest
+@testable import Git_Example
+
+class Git_ExampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
